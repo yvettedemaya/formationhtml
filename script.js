d577bdb0314219e0c29785aac7fd5df4d409d176
yvettedemaya <yvettedemaya@gemail.com>
2021-10-12 17:06:46 +0200

    mes essaie

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+  function valider(){
+ var  g =document.getElementById("b");
+ g.innerHTML="ggg";}
+         
+ 
+   var e = document.getElementById("ee");
+   e.addEventListener("mouseout",valider);
+       
+    
+
+   
+ /*function chercher() {
+     var b =document.getElementById('b');
+     b.style.fontSize = "100px"; 
+     b.style.color="blue"; 
+ }*/
+
+ function masquer {
+     document.getElementById('d');
+
+ }
+
+ vae r=""
